5f3b9cfdd6b36498d2e17d8a7b48a542bb44a922
JMP <josemarfyl@gmail.com>
2018-09-17 11:50:31 +0200

    test post with no mentions

--- tests/test_mentions.py
+++ tests/test_mentions.py
@@ -30,9 +30,11 @@ class TestMentions(TestCase):
         register(ModelWithCustomDescriptor, 'text', post_detect_mention_test_callback)
         user_mentioned_1 = get_user_model().objects.create(username='1')
         user_mentioned_2 = get_user_model().objects.create(username='2')
-        description = 'Hi <a class="mention" data-user="{}" href="/ecosystem/profile/{}/">@{}</a> and <a class="mention" data-user="{}" href="/ecosystem/profile/{}/">@{}</a>'.format(
-            user_mentioned_1.pk, user_mentioned_1.pk, user_mentioned_1.first_name,
-            user_mentioned_2.pk, user_mentioned_2.pk, user_mentioned_2.first_name)
+        description = 'Hi <a class="mention" data-user="{}", '\
+            'href="/ecosystem/profile/{}/">@{}</a> and , '\
+            '<a class="mention" data-user="{}" href="/ecosystem/profile/{}/">@{}</a>'.format(
+                user_mentioned_1.pk, user_mentioned_1.pk, user_mentioned_1.first_name,
+                user_mentioned_2.pk, user_mentioned_2.pk, user_mentioned_2.first_name)
 
         # DO ACTION
         self.instance = ModelWithCustomDescriptor.objects.create(text=description)
@@ -42,7 +44,7 @@ class TestMentions(TestCase):
         self.assertEqual(mocked_handler.call_count, 2)
 
     @patch('mentions.wrapper.MentionsWrapper.do_mention')
-    def test_model_registration_with_multiple_mentions(self, mocked_handler):
+    def test_model_registration_with_no_mentions(self, mocked_handler):
         # PREPARE DATA
         register(ModelWithCustomDescriptor, 'text', post_detect_mention_test_callback)
         user_mentioned = get_user_model().objects.create(username='1')
