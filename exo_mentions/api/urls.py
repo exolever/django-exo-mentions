b34236c9632d90e3bfda8f597c81b88290a4be29
Tomas <tomas@openexo.com>
2019-07-24 08:03:14 +0200

    Adding appname

--- exo_mentions/api/urls.py
+++ exo_mentions/api/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 
 from .views import SearchMentionAPIView
 
+app_name = 'mentions'
+
 urlpatterns = [
     url(r'^search/$', SearchMentionAPIView.as_view(), name='search'),
 ]
